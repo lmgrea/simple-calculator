0437e27825683412deb5356a43f27cea686235ca
Landon <lmgreatline5@gmail.com>
2023-10-03 17:26:45 -0500

    Implemented multiplication feature.

--- calculator.c
+++ calculator.c
@@ -3,6 +3,7 @@
 
 double add(double a, double b);
 double subtract(double a, double b);
+double multiply(double a, double b);
 
 int main () {
     double a, b, result;
@@ -13,6 +14,8 @@ int main () {
         printf("Result: %.2lf\n", result);
 
         printf("Subtract: %.2lf\n", subtract(a,b));
+
+        printf("Multiply: %.2lf\n", multiply(a,b));
 }
 
 double add(double a, double b) {
@@ -21,4 +24,8 @@ double add(double a, double b) {
 
 double subtract(double a, double b) {
     return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
 }
